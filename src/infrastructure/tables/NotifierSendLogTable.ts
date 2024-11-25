ff981dafbce92f8529f5680dd8d0ea7d6dcf333a
Danil <danil_ustinov@mail.ru>
2024-11-25 13:53:12 +0700

    remove IDeepPartial from tables

--- src/infrastructure/tables/NotifierSendLogTable.ts
+++ src/infrastructure/tables/NotifierSendLogTable.ts
@@ -1,6 +1,5 @@
 import {TableFromModel} from '@steroidsjs/nest/infrastructure/decorators/TableFromModel';
-import {IDeepPartial} from '@steroidsjs/nest/usecases/interfaces/IDeepPartial';
 import {NotifierSendLogModel} from '../../domain/models/NotifierSendLogModel';
 
 @TableFromModel(NotifierSendLogModel, 'notifier_send_log')
-export class NotifierSendLogTable implements IDeepPartial<NotifierSendLogModel> {}
+export class NotifierSendLogTable extends NotifierSendLogModel {}
--- src/infrastructure/tables/NotifierSendPushLogTable.ts
+++ src/infrastructure/tables/NotifierSendPushLogTable.ts
@@ -1,6 +1,5 @@
 import {TableFromModel} from '@steroidsjs/nest/infrastructure/decorators/TableFromModel';
-import {IDeepPartial} from '@steroidsjs/nest/usecases/interfaces/IDeepPartial';
 import {NotifierSendPushLogModel} from '../../domain/models/NotifierSendPushLogModel';
 
 @TableFromModel(NotifierSendPushLogModel, 'notifier_send_push_log')
-export class NotifierSendPushLogTable implements IDeepPartial<NotifierSendPushLogModel> {}
+export class NotifierSendPushLogTable extends NotifierSendPushLogModel {}
